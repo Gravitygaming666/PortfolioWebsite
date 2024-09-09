f08af4e399a6a6a1f2afc0bc33bcaf01dcb4d703
Gravitygaming666 <ryanmhill737@gmail.com>
2024-09-09 13:26:02 +0100

    add an image

--- Pages/GMACSpage.jsx
+++ Pages/GMACSpage.jsx
@@ -15,7 +15,7 @@ function GMACSpage() {
       <ProjectInfo imageSrc="ClientBrief.jpg" text="1. Info about the project" />
       <ProjectInfo imageSrc="TestPlan.jpg" text="2. Info about the project" />
       <ProjectInfo imageSrc="PowerPoint.jpg" text="3. Info about the project" />
-      <ProjectInfo imageSrc="WebsiteReportg.jpg" text="4. Info about the project" />
+      <ProjectInfo imageSrc="WebsiteReport.jpg" text="4. Info about the project" />
     </div>
     );
   }
